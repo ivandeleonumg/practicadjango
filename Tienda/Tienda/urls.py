6e4e0d7da6b7c1ca0d1d614589e999ca4a24e0c7
Iván de León <idl@t2mc.net>
2025-08-26 15:25:54 -0600

    add urls.py for home app and add urls.py for ventas app

--- Tienda/Tienda/urls.py
+++ Tienda/Tienda/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', include('Home.urls')),
+    path('ventas/', include('Ventas.urls')),
 ]
